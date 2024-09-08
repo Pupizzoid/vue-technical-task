ba7dafce098be3abba7ac07fa7b9ac6fd8c088b0
Vita  Kozlovska <vitakozlovska5@gmail.com>
2024-09-08 15:15:37 +0200

    Adds ArticleItem component

---
+++ src/components/ArticleItem.vue
@@ -0,0 +1,20 @@
+<script setup lang='ts'>
+const props = defineProps({
+  article: {type: Object, required: true }
+});
+
+const { article } = props;
+</script>
+
+<template>
+<li>
+  <a :href="article.url" target="_blank" class="item">
+    <span class="title">{{ article.title }}</span>
+    <span class="date">{{ article.date }}</span>
+  </a>
+</li>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/LatestUpdates.vue
+++ src/components/LatestUpdates.vue
@@ -1,6 +1,7 @@
 <script setup lang='ts'>
 import { ref } from 'vue'
 import type IArticle from '@/interfaces/Article.interface'
+import ArticleItem from '@/components/ArticleItem.vue'
 
 const articles = ref<IArticle[]>((window as any).LATEST_ARTICLES || []);
 Object.defineProperty(window, 'LATEST_ARTICLES', {
@@ -17,8 +18,17 @@ Object.defineProperty(window, 'LATEST_ARTICLES', {
 <div>
   <h2>Latest Updates</h2>
   <div class="latest-updates">
-
+    <div class="link-area" v-if="articles.length">
+      <ul>
+        <ArticleItem
+          v-for="article in articles"
+          :ref="article.url"
+          :article="article"/>
+      </ul>
+    </div>
+    <div v-else>Any updates</div>
   </div>
+
 </div>
 </template>
 
